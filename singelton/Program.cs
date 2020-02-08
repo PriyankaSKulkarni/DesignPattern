b1491038e4b4e08548125b64d45f3cbe57a6126f
PriyankaSKulkarni <kulkarnipriyanka33@gmail.com>
2020-02-08 15:03:11 +0530

    Add project files.

---
+++ singelton/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace singelton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parallel.Invoke(
+                () => PrintUser1Details(),
+                () => PrintUser2details(),
+                () => PrintUser3details()
+               
+                );
+            Console.ReadLine();
+        }
+        private static void PrintUser1Details()
+        {
+            Singleton user1 = Singleton.GetInstance;
+            user1.PrintDetails();
+        }
+        private static void PrintUser2details()
+        {
+            Singleton user2 = Singleton.GetInstance;
+            user2.PrintDetails();
+        }
+        private static void PrintUser3details()
+        {
+            Singleton user3 = Singleton.GetInstance;
+            user3.PrintDetails();
+        }
+        
+
+    }
+}
---
+++ singelton/Singleton.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace singelton
+{
+    public sealed class Singleton
+    {
+        private static int counter = 0;
+        private static readonly object Instancelock = new object();
+        private Singleton()
+        {            
+            Console.WriteLine("Singleton instance created...Counter Value " + counter.ToString());
+        }
+
+        ~Singleton() { }
+
+        private static Singleton instance = null;
+
+        public static Singleton GetInstance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (Instancelock)
+                    {
+                        if (instance == null)
+                        {
+                            instance = new Singleton();
+                        }
+                    }
+                }
+                return instance;
+            }
+        }
+        public void PrintDetails()
+        {
+            counter++;
+            
+            Console.WriteLine("Counter Value " + counter.ToString());
+            
+        }
+    }
+
+}
